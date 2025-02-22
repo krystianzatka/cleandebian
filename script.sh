fc2488943eb32ab3a46352a3bde22fa377953702
Krystian Zatka <kzatka@icloud.com>
2025-02-22 19:52:07 +0000

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+su
+sudo usermod -aG sudo $(whoami)
+
+sudo apt update
+sudo apt upgrade
+sudo apt remove gnome-2048 -y
+sudo apt remove gnome-clocks -y
+sudo apt remove gnome-maps -y
+sudo apt remove aisleriot -y
+sudo apt remove gnome-contacts -y
+sudo apt remove gnome-chess -y
+sudo apt remove five-or-more -y
+sudo apt remove four-in-a-row -y
+sudo apt remove hitori -y
+sudo apt remove gnome-klotski -y
+sudo apt remove cheese -y
+sudo apt-get remove --purge libreoffice* -y
+sudo apt remove lightsoff -y
+sudo apt remove gnome-mahjongg -y
+sudo apt remove gnome-mines -y
+sudo apt remove gnome-music -y
+sudo apt remove gnome-nibbles -y
+sudo apt remove quadrapassel -y
+sudo apt remove iagno -y
+sudo apt remove rhythmbox -y
+sudo apt remove gnome-robots -y
+sudo apt remove shotwell -y
+sudo apt remove gnome-sound-recorder -y
+sudo apt remove gnome-sudoku -y
+sudo apt remove swell-foop -y
+sudo apt remove tali -y
+sudo apt remove gnome-taquin -y
+sudo apt remove gnome-tetravex -y
+sudo apt remove transmission-gtk -y
